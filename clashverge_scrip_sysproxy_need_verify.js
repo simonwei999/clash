e8949e022e34b8c0576097bac2d062bfaec6d0d4
simonwei999 <simonwei999@outlook.com>
2026-06-02 13:49:47 +0800

    Update clashverge_scrip_sysproxy_need_verify.js

--- clashverge_scrip_sysproxy_need_verify.js
+++ clashverge_scrip_sysproxy_need_verify.js
@@ -22,7 +22,12 @@ const dnsConfig = {
   "cache-algorithm": "arc",
   "enhanced-mode": "fake-ip",
   "fake-ip-range": "198.18.0.1/16",
-  "fake-ip-filter": "https://raw.githubusercontent.com/simonwei999/clash/refs/heads/main/fakeipfilter_domain.list",
+  "fake-ip-filter": [
+    "+.lan", "+.local", "+.msftconnecttest.com", "+.msftncsi.com",
+    "localhost.ptlogin2.qq.com", "localhost.sec.qq.com", "+.in-addr.arpa",
+    "+.ip6.arpa", "time.*.com", "time.*.gov", "pool.ntp.org",
+    "localhost.work.weixin.qq.com"
+  ],
   "default-nameserver": ["223.5.5.5", "1.2.4.8"],
   "nameserver": [...foreignNameservers],
   "proxy-server-nameserver": [...domesticNameservers],
